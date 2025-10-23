8fdd53a2c4310edd760f5a209431745fa2a91049
Egg94417 <dicky94417@gmail.com>
2025-10-23 12:01:39 +0800

    second

---
+++ ch7-2-2 copy.php
@@ -0,0 +1,12 @@
+
+<?php 
+// 使用array()指定結合陣列的元素
+$arr = array( 
+   "sid"=>"C112181111", 
+   "name"=> "黃翊華", 
+   "shape"=>"三角形", 
+   100
+); 
+
+foreach($arr as $key=>$value) echo "$key=>$value <BR>";
+?>
